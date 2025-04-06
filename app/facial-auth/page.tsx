db3f4ac48b4664e0688f62ae5322a947230ce8b5
patty <2200080092@kluniversity.in>
2025-04-06 18:41:17 +0530

    2nd commit

--- app/facial-auth/page.tsx
+++ app/facial-auth/page.tsx
@@ -1,11 +1,13 @@
 import FacialAuthApp from "@/components/facial-auth-app"
 import ProtectedRoute from "@/components/protected-route"
-
+import { Suspense } from "react"
 export default function FacialAuthPage() {
   return (
     <ProtectedRoute>
       <main className="min-h-screen">
+      <Suspense fallback={<div>Loading...</div>}>
         <FacialAuthApp />
+        </Suspense>
       </main>
     </ProtectedRoute>
   )
