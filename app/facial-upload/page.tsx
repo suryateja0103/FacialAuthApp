41b7c73bb5ca1d61612ea54ac762cefc21e043aa
patty <2200080092@kluniversity.in>
2025-04-06 18:38:44 +0530

    updated code

--- app/facial-upload/page.tsx
+++ app/facial-upload/page.tsx
@@ -1,13 +1,19 @@
 import FacialAuthApp from "@/components/FacialUpload"
 import ProtectedRoute from "@/components/protected-route"
+import { Suspense } from "react"
+
 
 export default function FacialUploadPage() {
   return (
     <ProtectedRoute>
       <main className="min-h-screen">
-        <FacialAuthApp />
+        <Suspense fallback={<div>Loading...</div>}>
+          <FacialAuthApp />
+        </Suspense>
       </main>
     </ProtectedRoute>
   )
 }
+
+
 
